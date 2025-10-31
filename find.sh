1258e0c68a2d2683b9007fc64e19370d1a24042f
ziyuw-Plank <marxplank49@gmail.com>
2025-10-31 21:25:42 +0800

    Update find.sh

--- find.sh
+++ find.sh
@@ -3,4 +3,4 @@
 LAT=$1
 LON=$2
 
-curl -sS "https://raspy-smoke-d754.marxplank49.workers.dev/api/new/surrounding/car?latitude=${LAT}&longitude=${LON}" 
+curl -x http://36.147.78.166:443  "https://raspy-smoke-d754.marxplank49.workers.dev/api/new/surrounding/car?latitude=${LAT}&longitude=${LON}" 
